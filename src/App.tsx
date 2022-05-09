2479a00b25ae86585d0876fcf65d3b58750d205a
Ashley Dawson <ashley.dawson@deskpro.com>
2022-05-09 11:01:32 +0100

    Upgraded apps SDK

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { Main } from "./pages/Main";
 import "./App.css";
 
 import "flatpickr/dist/themes/light.css";
-import "notyf/notyf.min.css";
 import "tippy.js/dist/tippy.css";
 import "simplebar/dist/simplebar.min.css";
-import "flag-icon-css/css/flag-icons.min.css";
 
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
