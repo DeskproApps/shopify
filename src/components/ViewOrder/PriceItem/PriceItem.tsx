51835c53f36465401eed5bd74975e6271cd32bcd
ilia makarov <ilia.makarov@me.com>
2022-05-24 01:40:37 +0300

    Create markup ViewOrder page

---
+++ src/components/ViewOrder/PriceItem/PriceItem.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { H1, Stack } from "@deskpro/app-sdk";
+
+type Props = {
+    title: string,
+    price: string,
+} & Record<string, any>;
+
+const Container = styled(Stack)`
+  margin: 0 8px 10px 0;
+`;
+
+const PriceItem: FC<Props> = ({ title, price, ...props }) => (
+    <Container justify="space-between" {...props}>
+        <H1>{title}:</H1>
+        <H1>{price} USD</H1>
+    </Container>
+);
+
+export { PriceItem };
