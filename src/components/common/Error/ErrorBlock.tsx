8f8d0d3b71dc373e88f3e697efcb08128dc09d2b
ilia makarov <ilia.makarov@me.com>
2024-10-21 17:14:56 +0300

    More detailed error handling from the API

--- src/components/common/Error/ErrorBlock.tsx
+++ src/components/common/Error/ErrorBlock.tsx
@@ -1,21 +1,28 @@
 import styled from "styled-components";
 import { P5 } from "@deskpro/deskpro-ui";
-import type { FC, JSX } from "react";
-import type { Maybe } from "../../../types";
+import { DEFAULT_ERROR } from "../../../constants";
+import type { FC, ReactNode } from "react";
 
 type Props = {
-  text?: Maybe<string|JSX.Element|Array<string|JSX.Element>>,
+  text?: ReactNode;
 }
 
 const StyledErrorBlock = styled(P5)`
+  width: 100%;
   margin-bottom: 8px;
   padding: 4px 6px;
   border-radius: 4px;
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.red100};
+  box-sizing: border-box;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  white-space: normal;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
-const ErrorBlock: FC<Props> = ({ text = "An error occurred" }) => (
+const ErrorBlock: FC<Props> = ({ text = DEFAULT_ERROR }) => (
   <>
     {Array.isArray(text)
       ? text.map((msg, idx) => (<StyledErrorBlock key={idx}>{msg}</StyledErrorBlock>))
