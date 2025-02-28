7b023488c2c2cb107343efe55f16745c77b545fd
OJ Abba <phavorabba@gmail.com>
2025-02-28 16:42:04 +0000

    [Feat] Create "Login" page

---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,32 @@
+import { AnchorButton, H3, Stack } from "@deskpro/deskpro-ui"
+import { ErrorBlock } from "@/components/common"
+import { FC } from "react"
+import { useDeskproElements } from "@deskpro/app-sdk"
+import useLogin from "./useLogin"
+
+const LoginPage: FC = () => {
+    useDeskproElements(({ registerElement, clearElements }) => {
+        clearElements()
+        registerElement("refresh", { type: "refresh_button" })
+    })
+
+    const { onSignIn, authUrl, isLoading, error } = useLogin();
+
+    return (
+        <Stack padding={12} vertical gap={12} role="alert">
+            <H3>Log into your Shopify account.</H3>
+            <AnchorButton
+                disabled={!authUrl || isLoading}
+                href={authUrl || "#"}
+                loading={isLoading}
+                onClick={onSignIn}
+                target={"_blank"}
+                text={"Log In"}
+            />
+
+            {error && <ErrorBlock text={error}/>}
+        </Stack>
+    )
+}
+
+export default LoginPage
